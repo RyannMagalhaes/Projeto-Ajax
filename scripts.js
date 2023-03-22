b2186e7b7913a423440372deb39063d80ba4ef27
RyannMagalhaes <117134998+RyannMagalhaes@users.noreply.github.com>
2023-03-21 21:16:47 -0300

    teste script 2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function request(){
     var filename = "https://api.themoviedb.org/3/movie/popular?api_key=3d772faa9a4f43d2587f10a31e5d440a";
 
-    //fetch(filename).then(response => response.json()).then(dados => document.getElementById("teste").innerHTML = `${}`)
+    console.log(fetch(filename).then(response => response.json()).then(dados => document.getElementById("teste").innerHTML = `${dados[0].id}`));
 }
