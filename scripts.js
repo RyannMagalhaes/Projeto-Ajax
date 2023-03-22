53483b0a9a6d06223ac91a2c633641d2f92451bc
Ryann Cláudio <ryanncmm2002@gmail.com>
2023-03-22 16:36:46 -0300

    Consumo da API funcionando

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 function request(){
     var filename = "https://api.themoviedb.org/3/movie/popular?api_key=3d772faa9a4f43d2587f10a31e5d440a";
 
-    console.log(fetch(filename).then(response => response.json()).then(dados =>`${dados.results[0].title}`));
+    fetch(filename)
+    .then(response => response.json())
+    .then(dados =>
+        {   
+            var imagem = "https://image.tmdb.org/t/p/w500"+dados.results[0].poster_path
+            var titulo = dados.results[0].title
+            var popularide = dados.results[0].popularity
+            var data = dados.results[0].release_date
+            var resumo = dados.results[0].overview
+            console.log(titulo);
+            console.log(imagem);
+            console.log(popularide);
+            console.log(data);
+            console.log(resumo);
+        });
+        
+
 }
